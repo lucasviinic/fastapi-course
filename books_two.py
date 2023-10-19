4dae7d497725ab40db3746ca13e1e16102f78e0b
Lucas Vinícius <62507586+lucasviinic@users.noreply.github.com>
2023-10-18 21:33:10 -0300

    feat: apply pydantic

--- books_two.py
+++ books_two.py
@@ -1,4 +1,5 @@
 from fastapi import Body, FastAPI
+from pydantic import BaseModel
 
 
 app = FastAPI()
@@ -17,6 +18,13 @@ class Book:
         self.description = description
         self.rating = rating
 
+class BookRequest(BaseModel):
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: int
+
 BOOKS = [
     Book(1, 'Computer Science Pro', 'codingwithroby', 'A very nice book', 5),
     Book(2, 'Be FastWith Fast API', 'codingwithroby', 'A great book', 5),
@@ -31,5 +39,6 @@ async def read_all_books():
     return BOOKS
 
 @app.post("/create-book")
-async def create_book(book_request=Body()):
-    BOOKS.append(book_request)
+async def create_book(book_request: BookRequest):
+    new_book = Book(**book_request.model_dump())
+    BOOKS.append(new_book)
