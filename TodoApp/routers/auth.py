307629a12d6d671d6b6d607d6fd9765bb05a78f1
Lucas Vinícius <62507586+lucasviinic@users.noreply.github.com>
2023-11-16 17:17:39 -0300

    feat: apply authentication enhancements

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -13,11 +13,14 @@ import os
 from dotenv import load_dotenv
 
 
-router = APIRouter()
+router = APIRouter(
+    prefix='/auth',
+    tags=['auth']
+)
 load_dotenv()
 
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
-oauth2_bearer = OAuth2PasswordBearer(tokenUrl='token')
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl='auth/token')
 
 class Token(BaseModel):
     access_token: str
@@ -70,7 +73,7 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate user.')
 
 
-@router.post("/auth", status_code=status.HTTP_201_CREATED)
+@router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
     create_user_model = Users(
         email=create_user_request.email,
@@ -90,7 +93,7 @@ async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm,
                                  db: db_dependency):
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
-        return 'Failed Authentication'
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate user.')
     
     token = create_access_token(user.username, user.id, timedelta(minutes=20))
     
