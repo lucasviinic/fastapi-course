0beaa53e962001f6fd10cad6ec375b6b2d020233
Lucas Vinícius <62507586+lucasviinic@users.noreply.github.com>
2023-11-16 17:11:11 -0300

    feat: decode a jwt

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -1,14 +1,14 @@
 from datetime import datetime, timedelta
 from typing import Annotated
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from database import SessionLocal
 from models import Users
 from passlib.context import CryptContext
 from starlette import status
-from fastapi.security import OAuth2PasswordRequestForm
-from jose import jwt
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from jose import jwt, JWTError
 import os
 from dotenv import load_dotenv
 
@@ -17,6 +17,7 @@ router = APIRouter()
 load_dotenv()
 
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl='token')
 
 class Token(BaseModel):
     access_token: str
@@ -54,6 +55,20 @@ def create_access_token(username: str, user_id: int, expires_delta: timedelta):
     encode.update({'exp': expire})
 
     return jwt.encode(encode, os.getenv('SECRET_KEY'), algorithm=os.getenv('ALGORITHM'))
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
+    try:
+        payload = jwt.decode(token, os.getenv('SECRET_KEY'), algorithms=[os.getenv('ALGORITHM')])
+        username: str = payload.get('sub')
+        user_id: int = payload.get('id')
+
+        if username is None or user_id is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate user.')
+        
+        return {'username': username, 'id': user_id}
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate user.')
+
 
 @router.post("/auth", status_code=status.HTTP_201_CREATED)
 async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
