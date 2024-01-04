d30a8e71d373bc632c73e771a3fbae3d9bd27e0e
Lucas Vinícius <62507586+lucasviinic@users.noreply.github.com>
2024-01-04 20:10:21 -0300

    feat: add phone_number column using alembic

---
+++ TodoApp/alembic/versions/48ade542419d_create_phone_number_for_user_column.py
@@ -0,0 +1,26 @@
+"""Create phone number for user column
+
+Revision ID: 48ade542419d
+Revises: 
+Create Date: 2024-01-04 20:03:20.226601
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '48ade542419d'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.add_column('users', sa.Column('phone_number', sa.String(), nullable=True))
+
+
+def downgrade() -> None:
+    pass
